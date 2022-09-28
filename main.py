2b3b10fdfcbd0aed0dba6ff1a1d2c2ace039f185
Il444 <ifedorenko08@icloud.com>
2022-09-28 19:31:45 +0300

    Auto build APK file 64bit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
 class MortgageCalculator(MDApp):
-    def buils(self):
+    def build(self):
         return MDLabel(text="Hello, Mortgage Calculator", halign="center")
     
 MortgageCalculator().run()
